fb61925e60c53ae0efb99b979a47613bc27ac858
Kien Ngo <kenseverus@gmail.com>
2019-02-14 22:22:25 -0500

    Init on maze creation scene

---
+++ MazeAR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+    private Maze mazeInstance;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame()
+    {
+        // Create maze
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+    }
+
+    private void RestartGame()
+    {
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
