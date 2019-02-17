a3fee831f40a1f91dd094544b95fa3b06a0cd515
Kien Ngo <kenseverus@gmail.com>
2019-02-17 18:45:48 -0500

    Create maze cell in random directions

--- MazeAR/Assets/Scripts/Maze.cs
+++ MazeAR/Assets/Scripts/Maze.cs
@@ -8,6 +8,19 @@ public class Maze : MonoBehaviour
     public MazeCell cellPrefab;
     private MazeCell[,] cells;
     public IntVector2 size;
+    public IntVector2 RandomCoordinates
+    {
+        get
+        {
+            return new IntVector2(Random.Range(0, size.x), Random.Range(0, size.z));
+        }
+    }
+
+    public bool ContainsCoordinates (IntVector2 coordinate)
+    {
+        // Return true as long as the vector size falls within [0, 20] (20 here is the default size.x && size.z)
+        return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +38,15 @@ public class Maze : MonoBehaviour
     {
         WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
         cells = new MazeCell[size.x, size.z];
-        for (int x = 0; x < size.x; x++)
+        IntVector2 coordinates = RandomCoordinates;
+        // Keep creating new vector as long as [true]
+        while(ContainsCoordinates(coordinates) && GetCell(coordinates) == null)
         {
-            for (int z = 0; z < size.z; z++)
-            {
-                yield return delay;
-                CreateCell(new IntVector2(x, z));
-            }
+            // GetCell(coordinates) == null => To make sure to don't draw a cell twice, null means we haven't drawn any cell at that particular coordinate yet
+            yield return delay;
+            CreateCell(coordinates);
+            // Operator [+] in Invector2.cs
+            coordinates += MazeDirections.RandomValue.ToIntVector2();
         }
     }
 
@@ -42,5 +57,10 @@ public class Maze : MonoBehaviour
         newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
         newCell.transform.parent = transform;
         newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+    }
+    public MazeCell GetCell(IntVector2 coordinates)
+    {
+        // Get a MazeCell, for future validation in Generate()
+        return cells[coordinates.x, coordinates.z];
     }
 }
---
+++ MazeAR/Assets/Scripts/MazeDirection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MazeDirection
+{
+    North,
+    East,
+    South,
+    West
+}
+
+public static class MazeDirections
+{
+    public const int Count = 4; // 4 directions: N, S, W, E
+    public static MazeDirection RandomValue
+    {
+        get
+        {
+            // Randomly selection 1 out of 4 directions
+            return (MazeDirection)Random.Range(0, Count);
+        }
+    }
+
+    private static IntVector2[] vectors =
+    {
+        new IntVector2(0,1),
+        new IntVector2(1,0),
+        new IntVector2(0,-1),
+        new IntVector2(-1,0)
+    };
+
+    public static IntVector2 ToIntVector2 (this MazeDirection direction)
+    {
+        // Return an IntVector2 in a random direction, (not just by incresing either x or z)
+        return vectors[(int)direction];
+    }
+}
