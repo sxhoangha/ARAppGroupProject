ed264e2509279aed80008e02ac3618661e4483a5
Kien Ngo <kenseverus@gmail.com>
2019-02-18 05:04:26 -0500

    Script for camera's perspective

---
+++ MazeAR/Assets/Scripts/MainCamPerspective.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamPerspective : MonoBehaviour
+{
+    public Transform gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Rotate camera to look at the maze
+        transform.LookAt(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
