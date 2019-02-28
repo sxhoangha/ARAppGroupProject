5a774217b11c0740a558bb22552c738e91ff5162
Teru <lifetime1108@gmail.com>
2019-02-28 11:29:00 -0500

    UI Added

---
+++ Assets/Scripts/DropdownController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownController : MonoBehaviour
+{
+    public static DropdownController instance;
+    public Dropdown dropdown1;
+    public Dropdown dropdown2;
+    public Dropdown dropdown3;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ResetDropDown()
+    {
+        dropdown1.value = 0;
+        dropdown2.value = 0;
+        dropdown3.value = 0;
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("PlayGame");
+    }
+
+    public void MazeCreation()
+    {
+        SceneManager.LoadScene("MazeCreation");
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("MazeCreation"); // for now
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void HighScore()
+    {
+        SceneManager.LoadScene("HighScore");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Clear()
+    {
+        DropdownController.instance.ResetDropDown();
+    }
+
+}
