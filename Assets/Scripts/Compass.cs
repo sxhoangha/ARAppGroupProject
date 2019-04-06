2646c9603af706f4c62828924f6d01dd11e7761c
Teru <lifetime1108@gmail.com>
2019-04-06 13:11:42 -0400

    Bird view maze / Add compass in playmaze scene

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+    public Transform playerTransform;
+    Vector3 dir;
+
+    private void Update()
+    {
+        dir.z = playerTransform.eulerAngles.y;
+        transform.localEulerAngles = dir;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,15 +27,15 @@ public class GameManager : MonoBehaviour {
     //Add player
     private IEnumerator BeginGame()
     {
-        Camera.main.clearFlags = CameraClearFlags.Skybox;
-        Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
+        //Camera.main.clearFlags = CameraClearFlags.Skybox;
+        //Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
         mazeInstance = Instantiate(mazePrefab) as Maze;
         yield return StartCoroutine(mazeInstance.Generate());
         playerInstance = Instantiate(playerPrefab) as Player;
         playerInstance.transform.localScale = scale * playerInstance.transform.localScale;
         playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
-        Camera.main.clearFlags = CameraClearFlags.Depth;
-        Camera.main.rect = new Rect(0f, 0f, 0.4f, 0.4f);
+        //Camera.main.clearFlags = CameraClearFlags.Depth;
+        //Camera.main.rect = new Rect(0f, 0f, 0.4f, 0.4f);
     }
 
     //private void BeginGame () {
