ac0d1cb55b5bed69e4dc563046b48520afe060fc
Minseok Choi <minchoidev@gmail.com>
2019-04-08 01:16:24 -0400

    Added elapsed time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,34 +2,44 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	public Maze mazePrefab;
-
-	private Maze mazeInstance;
-
+    public Maze mazePrefab;
     public Player playerPrefab;
-
     public GameObject panelGameOver;    // Minseok 2019/04/07
+    public Text labelElpsdTime;    // Minseok 2019/04/07
 
+    private Maze mazeInstance;
     private Player playerInstance;
-
     private float scale = 0.1f; //will change later so that get from Maze script
+    private float startTime;
+    private bool isRunning = false;
 
-    private void Start () {
+    private void Start()
+    {
         StartCoroutine(BeginGame());
         //BeginGame();
-	}
-	
-	private void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			RestartGame();
-		}
+    }
+
+    private void Update()
+    {
+        // display elapsed time
+        if (isRunning == true)
+        {
+            labelElpsdTime.text = "Elapsed Time: " + (int)(Time.time - startTime) + "s";
+        }
+        // restart game by putting space key
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
 
         // check goalCount
         int goalCount = PlayerPrefs.GetInt("GoalCount");
         if (goalCount >= 1)
         {
+            isRunning = false;
             panelGameOver.SetActive(true);
             PlayerPrefs.DeleteKey("GoalCount");
             Debug.Log("Game Over!");
@@ -49,6 +59,8 @@ public class GameManager : MonoBehaviour {
         playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
         //Camera.main.clearFlags = CameraClearFlags.Depth;
         //Camera.main.rect = new Rect(0f, 0f, 0.4f, 0.4f);
+        isRunning = true;
+        startTime = Time.time;
     }
 
     //private void BeginGame () {
@@ -56,9 +68,10 @@ public class GameManager : MonoBehaviour {
     //	StartCoroutine(mazeInstance.Generate());
     //}
 
-    private void RestartGame () {
-		StopAllCoroutines();
-		Destroy(mazeInstance.gameObject);
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
         //BeginGame();
         if (playerInstance != null)
         {
